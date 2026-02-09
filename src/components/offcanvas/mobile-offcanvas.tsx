93ba761edb3b66be512ced704335c5d035e7b4c1
hiteshdevlpr <hiteshdevlpr@users.noreply.github.com>
2026-02-08 23:28:18 -0500

    Fixes mobile menu

--- src/components/offcanvas/mobile-offcanvas.tsx
+++ src/components/offcanvas/mobile-offcanvas.tsx
@@ -25,7 +25,7 @@ export default function MobileOffcanvas({ openOffcanvas, setOpenOffcanvas }: IPr
                 <div className="tp-offcanvas-wrapper">
                     <div className="tp-offcanvas-top d-flex align-items-center justify-content-between">
                         <div className="tp-offcanvas-logo">
-                            <Link href="#">
+                            <Link href="/" onClick={() => setOpenOffcanvas(false)}>
                                 <Image src="/logo-wo-shadow.png" alt="logo" width={85} height={100} />
                             </Link>
                         </div>
@@ -40,8 +40,8 @@ export default function MobileOffcanvas({ openOffcanvas, setOpenOffcanvas }: IPr
                     </div>
                     <div className="tp-offcanvas-main">
                         <div className="tp-offcanvas-content">
-                            <h3 className="tp-offcanvas-title">Hello There!</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+                            <h3 className="tp-offcanvas-title">BrightOne</h3>
+                            <p>Creative real estate marketing. Listings, branding, and video in the Greater Toronto Area.</p>
                         </div>
                         <div className="tp-main-menu-mobile d-xl-none">
                             <MobileMenus />
@@ -51,8 +51,8 @@ export default function MobileOffcanvas({ openOffcanvas, setOpenOffcanvas }: IPr
                                 {gallery_images.map((item, i) => (
                                     <div className="col-md-3 col-3" key={i}>
                                         <div className="tp-offcanvas-gallery-img fix">
-                                            <Link href="#">
-                                                <Image style={{ width: "100%", height: "auto" }} src={item} alt="gallery-img" />
+                                            <Link href="/portfolio" onClick={() => setOpenOffcanvas(false)}>
+                                                <Image style={{ width: "100%", height: "auto" }} src={item} alt="Portfolio" />
                                             </Link>
                                         </div>
                                     </div>
@@ -64,13 +64,13 @@ export default function MobileOffcanvas({ openOffcanvas, setOpenOffcanvas }: IPr
 
                             <ul>
                                 <li>
-                                    <Link href="tel:4164199689">(416) 419-9689</Link>
+                                    <Link href="tel:4164199689" onClick={() => setOpenOffcanvas(false)}>(416) 419-9689</Link>
                                 </li>
                                 <li>
-                                    <Link href="mailto:contact@brightone.ca">contact@brightone.ca</Link>
+                                    <Link href="mailto:contact@brightone.ca" onClick={() => setOpenOffcanvas(false)}>contact@brightone.ca</Link>
                                 </li>
                                 <li>
-                                    <Link href="#">Greater Toronto Area, ON</Link>
+                                    <Link href="/contact" onClick={() => setOpenOffcanvas(false)}>Greater Toronto Area, ON</Link>
                                 </li>
                             </ul>
                         </div>
@@ -78,16 +78,16 @@ export default function MobileOffcanvas({ openOffcanvas, setOpenOffcanvas }: IPr
                             <h3 className="tp-offcanvas-title sm">Follow Us</h3>
                             <ul>
                                 <li>
-                                    <Link href="https://www.facebook.com/BrightOneInc" target="_blank"><i className="fa-brands fa-facebook-f"></i></Link>
+                                    <Link href="https://www.facebook.com/BrightOneInc" target="_blank" rel="noopener noreferrer" onClick={() => setOpenOffcanvas(false)}><i className="fa-brands fa-facebook-f"></i></Link>
                                 </li>
                                 <li>
-                                    <Link href="https://www.instagram.com/brightoneinc" target="_blank"><InstagramTwo /></Link>
+                                    <Link href="https://www.instagram.com/brightoneinc" target="_blank" rel="noopener noreferrer" onClick={() => setOpenOffcanvas(false)}><InstagramTwo /></Link>
                                 </li>
                                 <li>
-                                    <Link href="https://www.linkedin.com/company/brightoneInc/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></Link>
+                                    <Link href="https://www.linkedin.com/company/brightoneInc/" target="_blank" rel="noopener noreferrer" onClick={() => setOpenOffcanvas(false)}><i className="fa-brands fa-linkedin-in"></i></Link>
                                 </li>
                                 <li>
-                                    <Link href="https://youtube.com/@brightoneca" target="_blank"><Youtube /></Link>
+                                    <Link href="https://youtube.com/@brightoneca" target="_blank" rel="noopener noreferrer" onClick={() => setOpenOffcanvas(false)}><Youtube /></Link>
                                 </li>
                             </ul>
                         </div>
